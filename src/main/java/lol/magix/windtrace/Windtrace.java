2520a2d57001a5ec6f6d165bbb7aa8cee2ec577d
KingRainbow44 <kobedo11@gmail.com>
2022-06-11 15:47:28 -0400

    Update plugin class

--- src/main/java/lol/magix/windtrace/Windtrace.java
+++ src/main/java/lol/magix/windtrace/Windtrace.java
@@ -1,7 +1,14 @@
 package lol.magix.windtrace;
 
+import emu.grasscutter.server.event.*;
+import emu.grasscutter.server.event.game.PlayerCreationEvent;
 import emu.grasscutter.plugin.Plugin;
-import lol.magix.windtrace.commands.WindyCommand;
+
+import lol.magix.windtrace.listeners.*;
+import lol.magix.windtrace.packet.handler.*;
+import lol.magix.windtrace.commands.*;
+
+import lol.magix.windtrace.utils.PacketUtils;
 
 public final class Windtrace extends Plugin {
     private static Windtrace instance;
@@ -18,13 +25,23 @@ public final class Windtrace extends Plugin {
     public void onLoad() {
         instance = this; // Set the instance to this plugin.
         
+        PacketUtils.windy(); // w i n d y .  Disable Grasscutter's packet protection.
+        
         this.getLogger().info("Windtrace was loaded.");
-    }
+    }   
 
     @Override
     public void onEnable() {
         // Register all commands.
         this.getHandle().registerCommand(new WindyCommand());
+        
+        // Register packet handlers.
+        this.getHandle().registerPacket(SelectSkillRequest.class);
+        
+        // Register event listeners.
+        new EventHandler<>(PlayerCreationEvent.class)
+                .listener(PlayerListener::onPlayerCreation)
+                .priority(HandlerPriority.LOW).register();
         
         this.getLogger().info("Windtrace was enabled.");
     }
