f79e2cdb1e3d41f42ca097c0f8b1c8aac149c437
KingRainbow44 <kobedo11@gmail.com>
2022-06-24 15:47:13 -0400

    Add additional logic

---
+++ src/main/java/lol/magix/windtrace/game/logic/TagLogic.java
@@ -0,0 +1,38 @@
+package lol.magix.windtrace.game.logic;
+
+import emu.grasscutter.server.event.game.ReceivePacketEvent;
+import lol.magix.windtrace.game.GameInstance;
+import lol.magix.windtrace.game.GameLogicHandler;
+import lol.magix.windtrace.player.WindtracePlayer;
+
+public final class TagLogic implements GameLogicHandler {
+    @Override
+    public void setup() {
+        
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void stop(boolean force) {
+
+    }
+
+    @Override
+    public void assignRole(WindtracePlayer player, GameInstance.Role role) {
+        
+    }
+
+    @Override
+    public void processPacket(WindtracePlayer player, String packetName, ReceivePacketEvent event) {
+
+    }
+}
